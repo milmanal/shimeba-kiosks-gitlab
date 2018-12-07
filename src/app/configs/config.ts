1a4221a54a92e3e4eb91e4c97bbff3d2e2bc78d0
Igor Tzvetaev <igor.tzvetaev@titanium-soft.com>
2018-12-07 13:02:22 +0200

    Updated All translation files

--- src/app/configs/config.ts
+++ src/app/configs/config.ts
@@ -9,7 +9,7 @@ export const Config = {
     homeMapUrl: "assets/map_99_north.jpg",
     directionMapUrl: "assets/map_99_north.jpg",
     arrowOnRouteUrl: 'assets/imgs/route-arrow.png',
-    drawTime: 2,
+    drawTime: 1.5,
     initZoom: 18,
     mobileInitZoom: 16.5,
     rotation: 93.5,
